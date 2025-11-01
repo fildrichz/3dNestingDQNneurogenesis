1721a9f7f63285b9ef59a2cde4f4e1fb9ed86eb6
Filip <fildrich@gmail.com>
2025-11-01 04:16:59 +0100

    3dBPP progress

--- main/main.py
+++ main/main.py
@@ -1,11 +1,11 @@
 from nesting.q4realbpp_loader import Q4RealBPPLoader
-from packing_with_dqn_q4real import train_dqn_q4realbpp, evaluate_dqn_q4realbpp
+from packing_with_dqn_q4real import train_q4real_dqn_sequential, evaluate_q4real_dqn_sequential
 
-instance = Q4RealBPPLoader.load_from_txt("nesting/3dBPP_11.txt")
+instance = Q4RealBPPLoader.load_from_txt("nesting/3dBPP_6.txt")
 
 # Train (CPU example); tweak episodes as you like
-agent = train_dqn_q4realbpp(instance, episodes=200, device="cuda", save_path=None)
+agent = train_q4real_dqn_sequential(instance, episodes=500, device="cuda", save_path=None)
 
 # Evaluate
-results = evaluate_dqn_q4realbpp(agent, instance, num_eval=5, visualize=True)
+results = evaluate_q4real_dqn_sequential(agent, instance, num_eval=5, visualize=True)
 print(results)
